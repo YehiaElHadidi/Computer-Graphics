6e23db9676a7a3731a71e517a3f35de28faec6d3
YehiaElHadidi <hadidiyehia@gmail.com>
2023-05-16 18:47:41 +0300

    sad

--- main.cpp
+++ main.cpp
@@ -8,125 +8,66 @@
 #include <windows.h>
 #include <bits/stdc++.h>
 #include <fstream>
+#include "Circle.h"
 
 using namespace std;
 
-int quadX, quadY;
+HMENU hMenu;
+
+void add_menu(HWND); //prototype
 
 
-void draw8points(HDC hdc, int xc, int yc, int a, int b, COLORREF color) {
-
-    SetPixel(hdc, xc + a, yc + b, color);
-    SetPixel(hdc, xc + b, yc + a, color);
-
-    SetPixel(hdc, xc - a, yc + b, color);
-    SetPixel(hdc, xc - b, yc + a, color);
-
-    SetPixel(hdc, xc - a, yc - b, color);
-    SetPixel(hdc, xc - b, yc - a, color);
-
-    SetPixel(hdc, xc + a, yc - b, color);
-    SetPixel(hdc, xc + b, yc - a, color);
-
-}
-
-void drawCircleDDA(HDC hdc, int xc, int yc, int r, COLORREF color) {
-
-    int x = 0;
-    int y = r;
-
-    int d = 1 - r;
-
-    while (x < y) {
-        if (d <= 0) {
-            d += 2 * x + 3;
-
-        } else {
-            d += 2 * (x - y) + 5;
-            y--;
-        }
-        x++;
-        draw8points(hdc, xc, yc, x, y, color);
+//shape color menu
+int shape_color() {
+    int c, color;
+    cout << "choose the color of the shape" << endl;
+    cout << "0 --> black" << endl;
+    cout << "1 --> white" << endl;
+    cout << "2 --> blue" << endl;
+    cout << "3 --> red" << endl;
+    cout << "4 --> yellow" << endl;
+    cout << "5 --> green" << endl;
+    cout << "6 --> purple" << endl;
+    cout << "7 --> orange" << endl;
+    cin >> c;
+    switch (c) {
+        case 0:
+            color = RGB(0, 0, 0);
+            break;
+        case 1:
+            color = RGB(255, 255, 255);
+            break;
+        case 2:
+            color = RGB(18, 82, 255);
+            break;
+        case 3:
+            color = RGB(255, 0, 0);
+            break;
+        case 4:
+            color = RGB(255, 255, 0);
+            break;
+        case 5:
+            color = RGB(40, 128, 34);
+            break;
+        case 6:
+            color = RGB(160, 32, 240);
+            break;
+        case 7:
+            color = RGB(255, 165, 0);
+            break;
+        default:
+            color = RGB(0, 0, 0);
+            break;
     }
-}
-
-int base = 0;
-int base1;
-
-void draw8pointsWithCircles(HDC hdc, int xc, int yc, int a, int b, int r, COLORREF color) {
-
-    SetPixel(hdc, xc + a, yc + b, color);
-    SetPixel(hdc, xc + b, yc + a, color);
-
-    SetPixel(hdc, xc - a, yc + b, color);
-    SetPixel(hdc, xc - b, yc + a, color);
-
-    SetPixel(hdc, xc - a, yc - b, color);
-    SetPixel(hdc, xc - b, yc - a, color);
-
-    SetPixel(hdc, xc + a, yc - b, color);
-    SetPixel(hdc, xc + b, yc - a, color);
-
-    if (xc + a >= (base + (r / 16))) {
-        base = (xc + a) + (r / 16);
-        for (int i = yc; i <= yc + b; i += (r / 8)) {
-            if (quadX < xc && quadY < yc) {
-                drawCircleDDA(hdc, xc - a, i - b, r / 16, RGB(128, 128, 128));
-            } else if (quadX > xc && quadY < yc)
-                drawCircleDDA(hdc, xc + a, i - b, r / 16, RGB(128, 128, 128));
-            else if (quadX > xc && quadY > yc)
-                drawCircleDDA(hdc, xc + a, i, r / 16, RGB(128, 128, 128));
-            else if (quadX < xc && quadY > yc)
-                drawCircleDDA(hdc, xc - a, i, r / 16, RGB(128, 128, 128));
-
-        }
-    }
-    if (xc + b <= base1 - (r / 16)) {
-        base1 = (xc + b) - (r / 16);
-        for (int i = yc; i <= yc + a; i += (r / 8)) {
-            if (quadX < xc && quadY < yc)
-                drawCircleDDA(hdc, xc - b, i - a, r / 16, RGB(128, 128, 128));
-            else if (quadX > xc && quadY < yc)
-                drawCircleDDA(hdc, xc + b, i - a, r / 16, RGB(128, 128, 128));
-            else if (quadX > xc && quadY > yc)
-                drawCircleDDA(hdc, xc + b, i, r / 16, RGB(128, 128, 128));
-            else if (quadX < xc && quadY > yc)
-                drawCircleDDA(hdc, xc - b, i, r / 16, RGB(128, 128, 128));
-
-        }
-    }
-
-
-
-}
-
-// the quarter that is going to be filled is depending on the place of the second click
-void drawCircleDDAWithCircles(HDC hdc, int xc, int yc, int r, COLORREF color) {
-
-    base = xc;
-    base1 = xc + r;
-    int x = 0;
-    int y = r;
-
-    int d = 1 - r;
-
-    while (x < y) {
-        if (d <= 0) {
-            d += 2 * x + 3;
-
-        } else {
-            d += 2 * (x - y) + 5;
-            y--;
-        }
-        x++;
-        draw8pointsWithCircles(hdc, xc, yc, x, y, r, color);
-    }
-
-
+    return color;
 }
 
 
-int x1, y11, x2, y2, cnt = 0;
+POINT p[4];
+int cnt = 0;
+COLORREF c = shape_color();
+
+
 
 
 /*  Declare Windows procedure  */
@@ -197,12 +138,12 @@ int WINAPI WinMain(HINSTANCE hThisInstance,
     return messages.wParam;
 }
 
-void save(HDC hdc){
+void save(HDC hdc) {
     ofstream out("file.txt");
     for (int i = 0; i < 529; ++i) {
         for (int j = 0; j < 336; ++j) {
-            COLORREF col = GetPixel(hdc,i,j);
-            if(col != 0x00FFFFFF){
+            COLORREF col = GetPixel(hdc, i, j);
+            if (col != 0x00FFFFFF) {
                 out << i << ' ' << j << ' ' << col << ' ';
             }
         }
@@ -212,50 +153,167 @@ void save(HDC hdc){
     out.close();
 
 }
-void load(HDC hdc){
+
+void clear(HDC hdc) {
+    for (int i = 0; i < 529; ++i) {
+        for (int j = 0; j < 336; ++j) {
+            COLORREF col = GetPixel(hdc, i, j);
+            if (col != 0x00FFFFFF) {
+                SetPixel(hdc, i, j, 0x00FFFFFF);
+            }
+        }
+    }
+    cout << "DONE" << endl;
+}
+
+
+void load(HDC hdc) {
     ifstream input("file.txt");
-    int x,y;
+    int x, y;
     COLORREF col;
-    while(input >> x){
+    while (input >> x) {
         input >> y;
         input >> col;
-        SetPixel(hdc,x,y,col);
+        SetPixel(hdc, x, y, col);
     }
     input.close();
 }
+
+int x1;
+int y11;
+int x2;
+int y2;
+int x3;
+int y3;
+int x4;
+int y4;
+int shape_no = 1;
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
     HDC hdc = GetDC(hwnd);
     switch (message)                  /* handle the messages */
     {
+        case WM_CREATE://create menu on top of screen
+            add_menu(hwnd);
+            break;
+        case WM_COMMAND:// function of every button in menu
+            switch (wParam) {
+                case 1://shapes button
+                    break;
+                case 2://clear button
+                    clear(hdc);
+                    break;
+                case 3:// save button
+                    save(hdc);
+                    break;
+                case 4:// load button
+                    load(hdc);
+                    break;
+                case 5://Exit button
+                    exit(0);
+                    break;
+                case 6:
+                    shape_no = 6;
+                    break;
+                case 7:
+                    shape_no = 7;
+                    break;
+                case 8:
+                    shape_no = 8;
+                    break;
+
+            }
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);       /* send a WM_QUIT to the message queue */
             break;
-
-
-        case WM_RBUTTONDOWN:
-                save(hdc);
-                break;
-
         case WM_LBUTTONDOWN:
-            load(hdc);
+            switch (shape_no) {
+                case 6:
+                    if (cnt == 0) {
+                        x1 = LOWORD(lParam);
+                        y11 = HIWORD(lParam);
+                        cnt++;
+                    } else if (cnt == 1) {
+                        x2 = LOWORD(lParam);
+                        y2 = HIWORD(lParam);
+                        int r = sqrt((y11 - y2) * (y11 - y2) + (x1 - x2) * (x1 - x2));
+                        drawCircle_quarterlinefill(hdc, x1, y11, r, c);
+                        cnt = 0;
+                    }
+                    break;
+                case 7:
+                    if (cnt % 2 == 0) {
+                                        x1 = LOWORD(lParam);
+                                        y11 = HIWORD(lParam);
+                                        cnt++;
+                                    }
+                    else if (cnt % 2 == 1) {
+                        quadX = LOWORD(lParam);
+                        quadY = HIWORD(lParam);
+                        int r = sqrt((y11 - quadY) * (y11 - quadY) + (x1 - quadX) * (x1 - quadX));
+
+                        drawCircleWithCircles(hdc, x1, y11, r, c);
+                        cnt= 0;
+                    }
+                    break;
+                    //draw line with midpoint
+                    case 8:
+                        if (cnt == 0) {
+                            x1 = LOWORD(lParam);
+                            y11 = HIWORD(lParam);
+                            cnt++;
+                        } else if (cnt == 1) {
+                            x2 = LOWORD(lParam);
+                            y2 = HIWORD(lParam);
+                            drawline(hdc, x1, y11, x2, y2, c);
+                            cnt = 0;
+                        }
+                        break;
+                        //draw hermite curve
+                        case 11:
+                            if (cnt == 3) {
+                                p[cnt].x = LOWORD(lParam);
+                                p[cnt].y = HIWORD(lParam);
+                                hermite(hdc, p, c);
+                                cnt = 0;
+
+                            } else {
+                                p[cnt].x = LOWORD(lParam);
+                                p[cnt].y = HIWORD(lParam);
+                                cnt++;
+                            }
+                            ReleaseDC(hwnd, hdc);
+                            break;
+
+
+            }
+
             break;
 
-            if (cnt % 2 == 0) {
-                x1 = LOWORD(lParam);
-                y11 = HIWORD(lParam);
-                cnt++;
-            } else if (cnt % 2 == 1) {
-                quadX = LOWORD(lParam);
-                quadY = HIWORD(lParam);
-                int r = sqrt((y11 - quadY) * (y11 - quadY) + (x1 - quadX) * (x1 - quadX));
-
-                drawCircleDDAWithCircles(hdc, x1, y11, r, RGB(128, 128, 128));
-                cnt++;
-            }
         default:                      /* for messages that we don't deal with */
             return DefWindowProc(hwnd, message, wParam, lParam);
     }
-
     return 0;
+}
+
+// the built in menu in window
+void add_menu(HWND hWnd) {
+    hMenu = CreateMenu();
+    HMENU hshapemenu = CreateMenu();
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR) hshapemenu, "Shapes");
+    AppendMenu(hMenu, MF_STRING, 2, "Clear");
+    AppendMenu(hMenu, MF_STRING, 3, "save");
+    AppendMenu(hMenu, MF_STRING, 4, "load");
+    AppendMenu(hMenu, MF_STRING, 5, "Exit");
+    AppendMenu(hshapemenu, MF_STRING, 6, "Circle with quad filled with lines");//
+    AppendMenu(hshapemenu, MF_STRING, 7, "Circle with quad filled with circles");//
+    AppendMenu(hshapemenu, MF_STRING, 8, "line");//
+    AppendMenu(hshapemenu, MF_STRING, 9, "ellipse");
+    AppendMenu(hshapemenu, MF_STRING, 10, "cardinal spline");
+    AppendMenu(hshapemenu, MF_STRING, 11, "hermit curve");
+
+
+
+    SetMenu(hWnd, hMenu);
 }
